40a0558c239012723e19ac4e49492d47d1f58a26
shipanroy10 <shipanroy10@gmail.com>
2020-10-17 19:51:08 +0600

    bit changed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydglb.mongodb.net/<creativeAgency>?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydglb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const serviceCollection = client.db("creativeAgency").collection("creativeAgency79");
